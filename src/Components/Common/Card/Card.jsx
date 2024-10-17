392b7a4d20cff1704300628db099fed87c71e8e7
muhaiminultasin <tasinmuhaiminul@gmail.com>
2024-10-18 05:56:43 +0600

    commit7 unusuall

--- src/Components/Common/Card/Card.jsx
+++ src/Components/Common/Card/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { cn } from '../../../lib/utils/utility'
 
-function Card({img, title, addr, price},i) {
+function Card({img, title, addr, price,className,classNameTitle, subClassName,priceClass},i) {
   return (
     <div className=''>
-      <div key={i} className='w-[306px] mx-auto p-2 mt-6 hover:shadow-lg transition delay-100 rounded cursor-pointer'>
+      <div key={i} className={cn(`w-[306px] mx-auto p-2 mt-6 hover:shadow-lg transition delay-100 rounded cursor-pointer`,className)}>
         <img src={img} alt="" />
         <div>
-            <h1 className='font-[700] text-base leading-[30px] mt-2'>{title}</h1>
-            <p className='font-[400] text-[14px] leading-[30px] text-primaryText'>{addr}</p>
-            <h2 className='font-[700] text-base leading-[30px]'>{price}</h2>
+            <h1 className={cn(`font-[700] text-base leading-[30px] mt-2`,classNameTitle)}>{title}</h1>
+            <p className={cn(`font-[400] text-[14px] leading-[30px] text-primaryText`,subClassName)}>{addr}</p>
+            <h2 className={cn(`font-[700] text-base leading-[30px]`, priceClass)}>{price}</h2>
         </div>
       </div>
     </div>
--- src/Components/Pages/Locations/Location.jsx
+++ src/Components/Pages/Locations/Location.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import SectionTitle from '../../Common/SectionTitle/SectionTitle'
 import InnerTitle from './InnerTitle'
+import Card from '../../Common/Card/Card'
 
 function Location() {
+
+    const IncidentLocation = [
+        {
+            id:3,
+            img:"./public/PBImage/card3.png",
+            title:"Whitechapel Rd.",
+            address:"Tulare County,  Los Angles, CA 23415",
+            price: "$1,456,654.00",
+          },
+          {
+            id:4,
+            img:"./public/PBImage/card4.png",
+            title:"Whitechapel Rd.",
+            address:"Tulare County,  Los Angles, CA 23415",
+            price: "$1,456,654.00",
+          },
+          {
+            id:5,
+            img:"./public/PBImage/card5.png",
+            title:"Whitechapel Rd.",
+            address:"Tulare County,  Los Angles, CA 23415",
+            price: "$1,456,654.00",
+          },
+          {
+            id:6,
+            img:"./public/PBImage/card6.png",
+            title:"Whitechapel Rd.",
+            address:"Tulare County,  Los Angles, CA 23415",
+            price: "$1,456,654.00",
+          },
+    ]
+
   return (
     <div className=''>
       <SectionTitle subTitle="Incidents - DR-4699 March 2023 Severe Storms" title="DR-4699 March 2023 Severe Storms" button1={"Sorted by : Date modified"} button2={"+ New Location"} btnClass={"w-[139px]" } btn1Width={"w-fit"} flexCol={"flex flex-col-reverse"}/>
@@ -10,8 +43,25 @@ function Location() {
             {/* first div */}
             <div>
                 <div className='border-b-2 pb-10'>
-                <InnerTitle img={"./public/PBImage/Location2.png"} title={"Tulare County,  Los Angles, CA 23415"} subtitle={"Location:"}/>
-                <InnerTitle img={"./public/PBImage/Cost.png"} title={"$60,607,456.00"} subtitle={"Approx. Cost:"} className={"border-b-2 pb-10"}/>
+                    <InnerTitle img={"./public/PBImage/Location2.png"} title={"Tulare County,  Los Angles, CA 23415"} subtitle={"Location:"}/>
+                    <InnerTitle img={"./public/PBImage/Cost.png"} title={"$60,607,456.00"} subtitle={"Approx. Cost:"} className={"border-b-2 pb-10"}/>
+                </div>
+
+                <div className='py-10 border-b-2'>
+                    <h1 className='font-[700] text-[14px] leading-[22px]'>Description</h1>
+                    <p className='font-[400] text-[16px] leading-[22px] text-primaryText'>Lorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werho</p>
+                </div>
+
+                <div className='py-10'>
+                    <h1 className='font-[700] text-[14px] leading-[22px]'>Location</h1>
+
+                    <div className='grid md:grid-cols-3'>
+                        {
+                            IncidentLocation.map( ({img,title,address,price},i) => {
+                                return <Card className={"w-fit"} img={img} classNameTitle={"text-[14px] leading-[21px]"} subClassName={"text-[12px]"} priceClass={"text-[14px]"} key={i} title={title} addr={address} price={price}/>
+                            }) 
+                        }
+                    </div>
                 </div>
             </div>
             {/* second-div */}
